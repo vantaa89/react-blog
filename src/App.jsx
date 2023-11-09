f47be4bdc42a76b0409849489a7446d701af85d0
Seojune Lee <leeseojune89@gmail.com>
2023-11-09 19:38:09 +0900

    Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import Axios from 'axios'
 
 import './style.css'
 
-import textpath from '../contents/main.md';
+import mainIntroPath from '../contents/main.md';
+import cvPath from '../contents/cv.md';
 
+const Header = (props) => {
+  const pages = [{link: '/', name: 'Home'}, {link: '/post', name: 'Posts'}, {link: '/about', name: 'About Me'}];
 
-const Header = () => {
   return (
     <nav className="header navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid container">
@@ -20,15 +22,12 @@ const Header = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="post">Posts</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about">About Me</a>
-            </li>
+            {pages.map((item, idx) => 
+              <li className="nav-item">
+                <a className={"nav-link"}
+                  aria-current="page" href={item.link}>{item.name}</a>
+              </li>
+            )}
           </ul>
         </div>
       </div>
@@ -46,34 +45,54 @@ const Footer = () => {
   );
 }
 
-import myImage from "../contents/profile_pic.jpg"
 const HomePage = () => {
   const [markdown, setMarkdown] = useState('');
 
   useEffect(() => {
-    Axios.get(textpath)
+    const timeOut = 5000;
+    Axios.get(mainIntroPath, {timeOut})
     .then((response) => setMarkdown(response.data));
   }, []);
 
   return (
     <div id="intro">
-      {/* <img src={myImage} alt={"my profile pic"} width="70%" /> */}
-      <MarkdownRenderer markdown={markdown} />
+      <h1>Hello!</h1>
+      <div class="row">
+        <div class="col-md-7">
+          <MarkdownRenderer markdown={markdown} />
+        </div>
+        <div class="col-md-3">
+          <img src="https://placeholder.co/300x200/" alt="my profile picture" />
+        </div>
+      </div>
     </div>
   );
 };
 
 const CurriculumVitae = () => {
+  const [cvMarkdown, setCVMarkdown] = useState('');
+
+  useEffect(() => {
+    const timeOut = 5000;
+    Axios.get(cvPath, {timeOut})
+    .then((response) => setCVMarkdown(response.data));
+  }, []);
+
   return (
     <div>
-      <h1>This is the CV Page</h1>
-      <p>CV</p>
+      <h1 id="cv-title">Seojune Lee (이서준)</h1>
+      <MarkdownRenderer markdown={cvMarkdown}/>
     </div>
   );
 }
 
-const PostPage = () => {
-
+const Posts = () => {
+  return (
+    <>
+      <h1>Posts</h1>
+      <p>This is the post page</p>
+    </>
+  )
 }
 
 const App = () => {
@@ -85,7 +104,7 @@ const App = () => {
           <div className="content">
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/posts" element={<PostPage />} />
+              <Route path="/post" element={<Posts />} />
               <Route path="/about" element={<CurriculumVitae />} />
             </Routes>
           </div>
